da5e73b6b44c3e3051518386511c5d0c4327851f
Oscar Forra Carbonell <oscarforraa@gmail.com>
2022-01-05 12:36:00 +0100

    theme

--- libs/theme.js
+++ libs/theme.js
@@ -4,7 +4,7 @@ import { mode } from '@chakra-ui/theme-tools';
 const styles = {
   global: () => ({
     body: {
-      bg: mode('#f0e7db', '#202023')
+      bg: mode('#f0e7db', '#f0e7db')
     }
   })
 };
